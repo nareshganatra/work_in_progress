ceabc183ab11726f5373d88c75d383ca27aabce8
naresh <nnarshmgg@gmail.com>
2017-02-09 01:56:16 -0800

    added bq_test() module

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 
 # Import the Flask Framework
 from flask import Flask
+from google.cloud import bigquery
+
 app = Flask(__name__)
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -10,6 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
+    bq_test()
     return 'Hello World!'
 
 
@@ -23,3 +26,43 @@ def page_not_found(e):
 def application_error(e):
     """Return a custom 500 error."""
     return 'Sorry, unexpected error: {}'.format(e), 500
+
+def bq_test():
+    '''
+    flow = flow_from_clientsecrets('client_secret_1055527281642-tefl57hmpug39uftv04t2de4lgj1vkck.apps.googleusercontent.com.json',
+                                   scope='https://www.googleapis.com/auth/bigquery',
+                                   redirect_uri='http://localhost:8080/')
+
+    auth_uri = flow.step1_get_authorize_url()
+    '''
+
+    # Instantiates a client
+
+    client = bigquery.Client()
+
+    query_results = client.run_sync_query("""
+    SELECT
+        APPROX_TOP_COUNT(corpus, 10) as title,
+        COUNT(*) as unique_words
+    FROM `publicdata.samples.shakespeare`;""")
+    # Use standard SQL syntax for queries.
+    # See: https://cloud.google.com/bigquery/sql-reference/
+    query_results.use_legacy_sql = False
+    query_results.run()
+    query_results.fetch_data()
+
+    # Drain the query results by requesting a page at a time.
+    page_token = None
+    while True:
+        rows, total_rows, page_token = query_results.fetch_data(
+            max_results=10,
+            page_token=page_token)
+
+        for row in rows:
+            print(row)
+
+        if not page_token:
+            break
+    print ("completed")
+
+
